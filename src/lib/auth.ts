33b31556610bf0f5db2231a0fb4e633c3791167c
Helcio <helcioanicio19@gmail.com>
2024-07-31 11:50:05 -0300

    Fix: Fixed error in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
@@ -12,3 +13,8 @@ export const authOptions: AuthOptions = {
     }),
   ],
 };
+function PrismaAdapter(
+  prismaClient: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+): import("next-auth/adapters").Adapter | undefined {
+  throw new Error("Function not implemented.");
+}
