fdb1b47116c304fbfd2ee58c7893e3644ee97b4d
Helcio <helcioanicio19@gmail.com>
2024-07-30 22:02:46 -0300

    Feat: Doing Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["fws-store.s3.sa-east-1.amazonaws.com", "utfs.io"],
   },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 export default nextConfig;
